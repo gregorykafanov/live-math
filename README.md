6e6508c8e8cf47c91d31baf1e8e13505ad83d419
greg <greg@mail.com>
2024-02-01 09:45:03 +0200

    +

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://github.com/mathjax/MathJax-examples/blob/master/advanced/interactive-sample.html
+
+https://stackoverflow.com/questions/58429951/second-call-to-mathjax-typeset-function-from-jquery-not-working
--- scripts.js
+++ scripts.js
@@ -3,9 +3,22 @@ $(document).ready(function () {
   //   let d = $("#formula").text(a);
   let i = 0;
   $("#formula").text("$$ x^1 $$");
+  MathJax.typeset(["#formula"]);
   setInterval(function () {
-    // $("#formula").text(`$$\\sum_{i=1}^{+\\infty} x^${i}$$`);
+    $("#formula").text(`$$\\sum_{i=1}^{+\\infty} x^{${i}}$$`);
     i += 1;
-    console.log("Hello");
+    MathJax.typeset(["#formula"]);
+    // console.log("Hello");
+    // console.log(MathJax);
   }, 1000);
 });
+// window.MathJax = {
+//   startup: {
+//     ready: () => {
+//       MathJax.startup.defaultReady();
+//       MathJax.startup.promise.then(() => {
+//         console.log("MathJax initial typesetting complete");
+//       });
+//     },
+//   },
+// };
