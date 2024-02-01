b05ff186fec90b6f3ff3756d75c1d74818603fb2
greg <greg@mail.com>
2024-02-01 10:22:00 +0200

    +

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
-$(document).ready(function () {
-  let i = 1;
-  $("#formula").text(`$$\\sum_{i=1}^{+\\infty} x^{${0}}$$`);
-  let k = 1;
-  let j = `\\sum_{i=1}^{+\\infty} x^{i}`;
-  MathJax.typeset();
-  MathJax.typeset(["#formula"]);
-  setInterval(function () {
-    $("#formula").text(`$$\\sum_{i=1}^{+\\infty} x^{${i}}$$`);
-    i += 1;
-    MathJax.typeset(["#formula"]);
-  }, 1000);
-  setInterval(function () {
-    $("#formula-2").text(`$$ ${j}  $$`);
-    if (k == 1) {
-      j += `=x^{${k}}`;
-    } else {
-      j += `+ x^{${k}}`;
-    }
-    k += 1;
-    MathJax.typeset(["#formula-2"]);
-  }, 1000);
-});
+// $(document).ready(function () {
+//   let i = 1;
+//   $("#formula").text(`$$\\sum_{i=1}^{+\\infty} x^{${0}}$$`);
+//   let k = 1;
+//   let j = `\\sum_{i=1}^{+\\infty} x^{i}`;
+//   MathJax.typeset();
+//   MathJax.typeset(["#formula"]);
+//   setInterval(function () {
+//     $("#formula").text(`$$\\sum_{i=1}^{+\\infty} x^{${i}}$$`);
+//     i += 1;
+//     MathJax.typeset(["#formula"]);
+//   }, 1000);
+//   setInterval(function () {
+//     $("#formula-2").text(`$$ ${j}  $$`);
+//     if (k == 1) {
+//       j += `=x^{${k}}`;
+//     } else {
+//       j += `+ x^{${k}}`;
+//     }
+//     k += 1;
+//     MathJax.typeset(["#formula-2"]);
+//   }, 1000);
+// });
